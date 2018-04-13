1152c37f66761a1038a0dcfa28bc8fafa127adfc
chrischung <code3110@gmail.com>
2018-04-14 01:12:58 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,27 @@
+#打印一个句子
+print ("I will now count my chickens:")
+#打印字符串Hens,并计算后面的数学式子，计算顺序根据先乘除后加减
+print ("Hens",25+30/6)
+#打印字符串Roosters，并计算后面的数学式子，%符号为取余运算
+print ("Roosters",100-25*3%4)
+#打印一行自字符串
+print ("Now I will count the eggs:")
+#计算数学式子
+print (3+2+1-5+4%2-1/4+6)
+#打印一行字符串，在双引号的范围内都表示为字符串
+print ("Is it true that 3+2<5-7?")
+#根据计算结果判断，返回true or false
+print (3+2<5-7)
+#打印字符串并计算数学式子，
+print ("What is 3+2?",3+2)
+#打印字符串并计算数学式子
+print ("What is 5-7?",5-7)
+#打印一行字符串
+print ("Oh,that's way is's False.")
+#打印一行字符串
+print ("How about some more.")
+#打印字符串，并对两数字进行比较判断，返回字符串和布尔值
+print ("Is it greater?",5>-2)
+print ("Is it greater or equal?",5>=-2)
+print ("Is it less or equal?",5<=-2)
+
---
+++ text.py
@@ -0,0 +1,4 @@
+inche =74 #inche
+temp =2.54
+cm =inche * temp
+print ("%d inche is %f cm." %(inche,cm))
